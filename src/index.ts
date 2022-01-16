5138d1bf5e96523d48fa73738e43d20bac926d34
Siberian Johnny <allidon93@gmail.com>
2022-01-16 18:46:28 +0700

    Homework 2 done

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,67 @@ import { renderSearchStubBlock } from './search-results.js'
 import { renderUserBlock } from './user.js'
 import { renderToast } from './lib.js'
 
+type UserType = {
+  userName: string,
+  avatarUrl: string
+};
+type favoritesAmountType = number;
+
+let userValue: UserType = {
+  userName: "Warren",
+  avatarUrl: "/img/avatar.png"
+};
+
+localStorage.user = JSON.stringify(userValue);
+localStorage.setItem('favoritesAmount', '3');
+
+function getUserData() {
+  const userData: unknown = JSON.parse(localStorage.user)
+  return userData as UserType;
+}
+const user = getUserData();
+
+function getFavoritesAmount() {
+  const favoritesData: unknown = localStorage.getItem('favoritesAmount');
+  return favoritesData as favoritesAmountType;
+}
+const favoritesAmount = getFavoritesAmount();
+
+interface SearchFormData {
+  City: string
+  CheckInDate: string
+  CheckOutDate: string
+  Price: number
+};
+
+function searchHandler() {
+  const form: HTMLFormElement = document.querySelector('form')
+  const cityData = <HTMLInputElement>document.getElementById('city')
+  const checkinData = <HTMLInputElement>document.getElementById('check-in-date')
+  const checkoutData = <HTMLInputElement>document.getElementById('check-out-date')
+  const maxpriceData = <HTMLInputElement>document.getElementById('max-price')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const searchData: SearchFormData = {
+      City: cityData.value,
+      CheckInDate: checkinData.value,
+      CheckOutDate: checkoutData.value,
+      Price: Number(maxpriceData.value),
+    }
+    search(searchData)
+  })
+}
+
+function search(searchData: SearchFormData) {
+  console.log(searchData)
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock('Warren', '/img/avatar.png', 1)
+  renderUserBlock(user.userName, user.avatarUrl, favoritesAmount)
   renderSearchFormBlock()
   renderSearchStubBlock()
+  searchHandler()
   renderToast(
     { text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' },
     { name: 'Понял', handler: () => { console.log('Уведомление закрыто') } }
